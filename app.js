00ce4a3f6919b0a03e97728943b1f5f8599fb0fc
JHud <219741840+JamHudson@users.noreply.github.com>
2026-01-29 14:23:26 -0800

    App.js file

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ const app = express();
 
 //define a port number
 const PORT  = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to My Ice Cream Shop!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at
+        http://localhost:${PORT}`)
+});
