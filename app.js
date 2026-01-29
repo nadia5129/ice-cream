8233704632bd489559a09ffaff8a87ea8fb5a95a
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-01-29 14:21:43 -0800

    created app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+//import the express module
+const express = require('express');
+
+//create an instance of express application
+const app = express();
+
+//define a port number
+const PORT  = 3000;
